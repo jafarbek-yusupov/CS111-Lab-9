9439b29a1c91e80f989149bb607ada2c01137afa
Jafarbek Yusupov <info.jafarbekyusupov@gmail.com>
2025-10-24 09:10:34 +0500

    p1 ntm

---
+++ p1.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val) {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+void preorder(Node* root) {
+    if (root == nullptr) return;
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+void inorder(Node* root) {
+    if (root == nullptr) return;
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+void postorder(Node* root) {
+    if (root == nullptr) return;
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
+}
+void levelOrder(Node* root) {
+    if (root == nullptr) return;
+    queue<Node*> q;
+    q.push(root);
+    while (!q.empty()) {
+        Node* current = q.front();
+        q.pop();
+        cout << current->data << " ";
+
+        if (current->left != nullptr) {
+            q.push(current->left);
+        }
+        if (current->right != nullptr) {
+            q.push(current->right);
+        }
+    }
+}
+int main() {
+    Node* root = new Node(1);
+    root->left = new Node(5);
+    root->right = new Node(8);
+    root->left->left = new Node(4);
+    root->left->right = new Node(7);
+    root->right->left = new Node(90);
+    cout << "Preorder: ";
+    preorder(root);
+    cout << endl;
+    cout << "Inorder: ";
+    inorder(root);
+    cout << endl;
+    cout << "Postorder: ";
+    postorder(root);
+    cout << endl;
+    cout << "LevelOrder: ";
+    levelOrder(root);
+    cout << endl;
+    return 0;
+}
